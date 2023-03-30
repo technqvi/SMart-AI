01b036b6a4c9671deb66d9d01f738e6461200943
technqvi <technqvi_gangster@hotmail.com>
2023-03-30 23:17:00 +0700

    ok

---
+++ Model-TF_Keras/tf1_incident_severity/main.py
@@ -0,0 +1,245 @@
+from google.cloud import bigquery
+import tensorflow as tf
+
+import pandas as pd
+
+import os
+import json
+from datetime import date,datetime,timedelta
+
+import functions_framework
+
+from google.cloud.exceptions import NotFound
+from google.api_core.exceptions import BadRequest
+
+@functions_framework.http
+def predict_incident_severity_by_tf(request):
+
+
+# In[49]:
+
+
+    load_model_option=os.environ.get('load_model_option', '2')
+    predict_from_date=os.environ.get('predict_from_date', '')
+    
+    # load_model_option='1'
+    # predict_from_date=''
+
+    if load_model_option=='1':
+      PATH_FOLDER_ARTIFACTS="model"  
+    elif load_model_option=='2':
+      PATH_FOLDER_ARTIFACTS="gs://tf1-incident-pongthorn/model"
+    else:
+      raise Exception("Allow you to set 1 for local and 2 for google storage")
+
+    print(f"Load data from {PATH_FOLDER_ARTIFACTS}")
+
+
+    # In[50]:
+
+
+    table_id = "pongthorn.SMartML.new_incident"
+    predictResult_table_id="pongthorn.SMartML.new_result_prediction_incident"
+
+
+    unUsedColtoPredict=['severity','id','severity_id','severity_name','imported_at']
+
+
+    # In[51]:
+
+
+    mapping_file="incident_sevirity_to_class.json"
+    # with open(mapping_file, 'r') as json_file:
+    #      map_sevirity_to_class= json.load(json_file)
+    # print(map_sevirity_to_class)
+
+    map_sevirity_to_class={'Cosmatic': 0, 'Minor': 1, 'Major': 2, 'Critical': 3}
+    print(map_sevirity_to_class)
+
+
+    # In[52]:
+
+
+    # Get today's date
+    prediction_datetime=datetime.now()
+
+    today = date.today()
+
+    # Yesterday date
+    if predict_from_date=='':
+     yesterday = today - timedelta(days = 1)
+     str_yesterday=yesterday.strftime('%Y-%m-%d')
+    else:
+     str_yesterday=predict_from_date
+
+    str_today=today.strftime('%Y-%m-%d')
+
+    print(f"Get data between {str_yesterday} to {str_today} to predict sevirity level")
+
+
+    # In[53]:
+
+
+    def load_data_bq(sql:str):
+     client_bq = bigquery.Client()
+     query_result=client_bq.query(sql)
+     df=query_result.to_dataframe()
+     return df
+
+
+    # In[54]:
+
+
+    sql=f"""
+    SELECT *  FROM `{table_id}` 
+    WHERE DATE(imported_at) >= '{str_yesterday}' and DATE(imported_at) < '{str_today}' 
+
+    """
+    #LIMIT 2
+    dfNewData=load_data_bq(sql)
+    dfNewData=dfNewData.drop_duplicates(subset=['id'],keep='first')
+
+    dfNewData.insert(2, 'severity', dfNewData['severity_name'].map(map_sevirity_to_class),True)
+
+
+    print(dfNewData.info())
+    print(dfNewData)
+
+
+    # In[55]:
+
+
+    try:
+        model = tf.keras.models.load_model(PATH_FOLDER_ARTIFACTS)    
+        print(f"Load from {PATH_FOLDER_ARTIFACTS}")
+        print(model.summary())
+    except Exception as error:
+      print(str(error))
+      raise error
+
+
+    # In[56]:
+
+
+    pdPrediction=pd.DataFrame(columns=['_id','predict_severity','prob_severity'])
+
+    for  row_dict in dfNewData.to_dict(orient="records"):
+          incident_id=row_dict['id']
+          print(f"{incident_id} - {row_dict['severity']}({row_dict['severity_name']})") 
+          for key_removed in unUsedColtoPredict:
+           row_dict.pop(key_removed)
+          # print(row_dict)  
+
+          input_dict = {name: tf.convert_to_tensor([value]) for name, value in row_dict.items()}
+
+
+          predictionResult = model.predict(input_dict)
+          result_str=','.join([ str(prob) for prob in predictionResult[0]])  
+          print(result_str)   
+
+          prob = tf.nn.softmax(predictionResult)
+          prob_pct=(100 * prob)  
+          _class = tf.argmax(predictionResult,-1).numpy()[0]
+
+          dictPrediction={'_id':incident_id, 'predict_severity':_class,'prob_severity':result_str} 
+          pdPrediction =pd.concat([pdPrediction,pd.DataFrame.from_dict([dictPrediction])] )
+
+          print(f"{prob_pct} %   as {_class}")     
+          print("======================================================================================")
+
+    dfPredictData=pd.merge(dfNewData,pdPrediction,how='inner',left_on='id',right_on='_id')
+    dfPredictData=dfPredictData.drop(columns=['_id'])
+    dfPredictData['predict_severity']=dfPredictData['predict_severity'].astype('int')
+    dfPredictData=dfPredictData[['id','prob_severity','predict_severity','severity']]
+    dfPredictData['prediction_item_date']= datetime.strptime(str(yesterday), '%Y-%m-%d')
+    dfPredictData['prediction_datetime']=prediction_datetime
+
+
+    # In[57]:
+
+
+    print(dfPredictData.info())
+    print(dfPredictData)
+
+
+    # In[58]:
+
+
+    #https://cloud.google.com/bigquery/docs/samples/bigquery-create-table#bigquery_create_table-python
+
+    try:
+        client = bigquery.Client()
+        client.get_table(predictResult_table_id)  # Make an API request.
+        print("Predict Result Table {} already exists.".format(predictResult_table_id))
+    except Exception as ex:
+        schema = [
+        bigquery.SchemaField("id", "INTEGER", mode="REQUIRED"),
+        bigquery.SchemaField("prob_severity", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("predict_severity", "INTEGER", mode="REQUIRED"),
+        bigquery.SchemaField("severity", "INTEGER", mode="REQUIRED"),    
+        bigquery.SchemaField("prediction_item_date", "DATETIME", mode="REQUIRED"),    
+        bigquery.SchemaField("prediction_datetime", "DATETIME", mode="REQUIRED") 
+        ]
+
+        table = bigquery.Table(predictResult_table_id,schema=schema)
+        table.time_partitioning = bigquery.TimePartitioning(
+        type_=bigquery.TimePartitioningType.DAY,field="prediction_item_date")
+
+        table = client.create_table(table)  # Make an API request.
+
+        print(
+            "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+        )
+
+
+    # In[59]:
+
+
+    def loadDataFrameToBQ():
+        try:
+            job_config = bigquery.LoadJobConfig(
+                write_disposition="WRITE_APPEND",
+            )
+
+            job = client.load_table_from_dataframe(
+                dfPredictData, predictResult_table_id, job_config=job_config
+            )
+            job.result()  # Wait for the job to complete.
+            print("Total Prediction ML ", len(dfPredictData), "Imported igquery successfully")
+
+        except BadRequest as e:
+            print("Bigquery Error\n")
+            for e in job.errors:
+                print('ERROR: {}'.format(e['message']))
+
+    try:
+        loadDataFrameToBQ()
+    except Exception as ex:
+        raise ex
+
+
+    # In[ ]:
+
+
+    return 'ok'
+
+
+# In[ ]:
+
+
+# if __name__ == "__main__":
+#  result=predict_incident_severity_by_tf(None)
+#  print(result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
